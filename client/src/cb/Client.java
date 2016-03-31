bfd065c09fc2ad275948b4eb0ad75f921cd8eadc
Lukas Mayer <lmayer@student.tgm.ac.at>
2016-03-31 10:14:41 +0200

    added working makefile and Client

--- client/src/cb/Client.java
+++ client/src/cb/Client.java
@@ -1,18 +1,12 @@
-package org.jacorb.demo.bidir;
+package cb;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Properties;
-
-import org.omg.BiDirPolicy.BIDIRECTIONAL_POLICY_TYPE;
-import org.omg.BiDirPolicy.BOTH;
-import org.omg.BiDirPolicy.BidirectionalPolicyValueHelper;
-import org.omg.CORBA.Any;
-import org.omg.CORBA.Policy;
 import org.omg.PortableServer.IdAssignmentPolicyValue;
 import org.omg.PortableServer.ImplicitActivationPolicyValue;
 import org.omg.PortableServer.LifespanPolicyValue;
 import org.omg.PortableServer.POA;
+import org.omg.CosNaming.*;
+import org.omg.CORBA.*;
+import org.omg.CORBA.Object;
 
 /**
  * Client.java
@@ -24,27 +18,27 @@ import org.omg.PortableServer.POA;
  * @author Nicolas Noffke, Lukas Mayer
  */
 
-public class Client extends ClientCallbackPOA
+public class Client extends CallBackPOA
 {
     public Client ()
     {
     }
 
-    public void hello (String message)
+    public void call_back (String message)
     {
         System.out.println ("Client callback object received hello message >"
                 + message + '<');
     }
 
     /**
-    * obtains a reference over a naming service and also starts a POA 
+    * obtains a reference over a naming service and also starts a POA
     * "registers" the callback at the server and shuts the server down if a user input happens
     */
     public static void main (String[] args) throws Exception
     {
         org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init (args, null); //args = address of the naming service
 
-	Object o = orb.resolve_initial_references("NameService"); 
+	Object o = orb.resolve_initial_references("NameService");
 	NamingContextExt rootContext = NamingContextExtHelper.narrow(o); //casting
 	NameComponent[] name = new NameComponent[2];
 	name[0] = new NameComponent("test","my_context");
@@ -56,11 +50,12 @@ public class Client extends ClientCallbackPOA
         root_poa.the_POAManager().activate(); //starting the root POA
 
 	//Creating and casting a new Callback-Object
-        CallBack ccb = ClientCallbackHelper.narrow(root_poa.servant_to_reference(new Client()));
-	server.register(ccb, "worked! methods gets invoked every 4 seconds", 4);
+        CallBack ccb = CallBackHelper.narrow(root_poa.servant_to_reference(new Client()));
+	server.register(ccb, "worked! methods gets invoked every 4 seconds", (short) 4);
 
 	while (System.in.read() == '\n'){
         	server.shutdown();
+		System.exit(1);
 	}
     }
 }// Client
